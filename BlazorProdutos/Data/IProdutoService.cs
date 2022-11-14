5188a7b78f15781b844d69daa3a395d9a21f4e96
Fabio <fabio@ifabio.com.br>
2022-11-14 15:24:35 -0300

    BlazorDapper

---
+++ BlazorProdutos/Data/IProdutoService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlazorProdutos.Data
+{
+    public interface IProdutoService
+    {
+        Task<int> Create(Produto produto);
+        Task<int> Delete(int Id);
+        Task<int> Update(Produto produto);
+        Task<Produto> GetById(int Id);
+        Task<List<Produto>> ListAll();
+    }
+}
---
+++ BlazorProdutos/Data/Produto.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorProdutos.Data
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string Imagem { get; set; }
+        public decimal Preco { get; set; }
+        public int Estoque { get; set; }
+    }
+}
